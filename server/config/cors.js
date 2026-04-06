e8be7242d8436a27a7f2349c3e06feb5494a7896
HoangHuy2501 <huy04.developer@gmail.com>
2026-04-06 22:03:28 +0700

    cors server

--- server/config/cors.js
+++ server/config/cors.js
@@ -2,7 +2,7 @@
 var cors=require('cors');
 module.exports=cors({
   // origin: '*', // hoặc cụ thể: http://localhost:3000
-   origin: ['http://localhost:5173', "https://chinhchinh127.github.io/KLTN-FWMS/"],
+   origin: ['http://localhost:5173', "https://system-waste-less-ai-1.onrender.com"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With',],
 //   credentials: true // Cho phép gửi cookie
