a69c69d7c57b350998a89859c5c5b8b82250ad84
HoangHuy2501 <huy04.developer@gmail.com>
2026-04-12 13:50:03 +0700

    validator chat

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const ChatController = require('../controller/ChatController');
-router.post('/send', ChatController.sendChatUser);
+const validator = require('../middleware/validator/chat');
+router.post('/send',validator, ChatController.sendChatUser);
 router.get('/get-message/:MessID', ChatController.RoomMessage);
 router.put('/mark-as-read/:messageID', ChatController.markAsRead);
 router.get('/list-message', ChatController.ListchatMessageTogether);
