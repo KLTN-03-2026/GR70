026d0a7f81c2896011376cc82d5d49a1e9841241
HoangHuy2501 <huy04.developer@gmail.com>
2026-05-19 22:06:51 +0700

    fix history

--- server/repository/DailyRepository.js
+++ server/repository/DailyRepository.js
@@ -400,7 +400,7 @@ async SumWasteByMonth(brandID, month = null) {
     ],
     raw: true,
   });
-  return result || 0;
+  return Number(result) || 0;
 }
   // danh sách lãng phí món ăn theo nguyên liệu
   async ListWasteByIngredient(brandID, month) {
