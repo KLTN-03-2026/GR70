89707ba07a1a3951f005c7057f6ab27338b9f6ab
Lequochung28 <hungle280204@gmail.com>
2026-05-20 10:25:18 +0700

    Fix/thong tin (#72)
    * conect api
    
    * update profile feature

--- KLTN-FWMS/src/components/Layout/SiderbarAdmin.jsx
+++ KLTN-FWMS/src/components/Layout/SiderbarAdmin.jsx
@@ -27,7 +27,7 @@ export const SiderbarAdmin = () => {
         { name: "Bảng điều khiển", icon: "dashboard", path: "/admin/dashboard" },
         { name: "Quản lý tài khoản", icon: "user_attributes", path: "/admin/user" },
         { name: "Quản lý cửa hàng", icon: "storefront", path: "/admin/storeManager" },
-        { name: "Thống kê hệ thống", icon: "analytics", path: "/admin/statistical" },
+        
     ];
 
     const [openModal, setOpenModal] = useState(false);
